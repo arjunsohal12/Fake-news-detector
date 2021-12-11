c4a31f899abcc054c5759f06874754a516794ec0
arjunsohal12 <38443000+arjunsohal12@users.noreply.github.com>
2021-12-11 16:30:30 -0500

    Add files via upload

---
+++ new.py
@@ -0,0 +1,91 @@
+import re
+import numpy as np
+import pandas as pd
+from gensim.models import KeyedVectors
+from nltk.stem.porter import PorterStemmer
+from keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, LSTM, Conv1D, MaxPool1D
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D, Bidirectional
+import confusion_matrix as conmat
+from sklearn import metrics
+
+ps = PorterStemmer()
+
+# Using word embeddings based off of the same training data might yield better results?, but less general purpose
+# than google database When user inputs new data, could also train new embeddings by appending the new data to our
+# corpus
+model = KeyedVectors.load_word2vec_format("GoogleNews-vectors-negative300.bin", binary=True, limit=10000)
+df = pd.read_csv('train.csv')
+df = df.dropna()
+messages = df.copy()
+messages.reset_index(inplace=True)
+messages = messages
+y = df['label']
+y = y
+tokenizer = Tokenizer()
+
+wordnet_lemmatizer = WordNetLemmatizer()
+
+
+def preprocess(messages):
+    output = []
+    for i in range(0, len(messages)):
+        review = re.sub('[^a-zA-Z]', ' ', messages['text'][i])
+        review = review.lower()
+        review = review.split()
+        review = [wordnet_lemmatizer.lemmatize(word) for word in review if not word in stopwords.words('english')]
+
+        output.append(review)
+    return output
+
+
+# Try dropping values with less than 20 words?
+
+corpus = preprocess(messages)
+tokenizer.fit_on_texts(corpus)
+X = tokenizer.texts_to_sequences(corpus)
+
+print(X[0][:10])
+
+maxlen = 500
+X = pad_sequences(X, maxlen=maxlen)
+embedding_size = 300
+index = tokenizer.word_index
+vocab_size = len(tokenizer.word_index) + 1
+
+matrix = np.zeros((vocab_size, embedding_size))
+for word, i in index.items():
+    try:
+        matrix[i] = model[word]
+    except KeyError:
+        matrix[i] = np.random.normal(0, 0, embedding_size)
+
+print(matrix)
+model = Sequential()
+
+model.add(Embedding(input_dim=vocab_size,
+                    output_dim=embedding_size,
+                    weights=[matrix],
+                    input_length=maxlen,
+                    trainable=False))
+model.add(MaxPool1D())
+model.add(LSTM(units=128))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+model.fit(X_train, y_train, validation_split=0.1, epochs=12)
+pred = (model.predict(X_test) >= 0.5).astype("int")
+score = accuracy_score(y_test, pred)
+
+print("accuracy: %0.3f" % score)
+cm = metrics.confusion_matrix(y_test, pred)
+poo = conmat.plot_confusion_matrix(cm, classes=["FAKE", "REAL"])
