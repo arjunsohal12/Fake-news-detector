38dec659275916b2cd9ac13e43669677fe584869
arjunsohal12 <38443000+arjunsohal12@users.noreply.github.com>
2021-12-12 12:15:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -83,4 +83,4 @@ score = accuracy_score(y_test, pred)
 
 
 cm = metrics.confusion_matrix(y_test, pred)
-poo = conmat.plot_confusion_matrix(cm, classes=["FAKE", "REAL"])
+matrix = conmat.plot_confusion_matrix(cm, classes=["FAKE", "REAL"])
